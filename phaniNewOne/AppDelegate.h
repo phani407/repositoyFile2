fb3e732a5b72855cdb1e664da0d6f8e11e17e82b
skvalibabu <practice@grits-mac-mini-4.local>
2016-12-07 11:31:08 +0530

    Initial Commit

---
+++ phaniNewOne/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  phaniNewOne
+//
+//  Created by Practice on 07/12/16.
+//  Copyright © 2016 Practice. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ phaniNewOne/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  phaniNewOne
+//
+//  Created by Practice on 07/12/16.
+//  Copyright © 2016 Practice. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
